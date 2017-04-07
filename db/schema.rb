8ff480f34863ccbc274ad6dbc8486d69714d3a82
jedney0901 <joshuaedney@Joshuas-MacBook-Pro.local>
2017-04-07 15:44:20 +1000

    Added Carrierwave Gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407020241) do
+ActiveRecord::Schema.define(version: 20170407054229) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170407020241) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
